f36068399247a88fccf598fc23db7bd22334744e
aws-devops26 <srivalli26.najara@gmail.com>
2024-08-16 21:29:04 +0530

    roboshop shell scripts

--- mongodb.sh
+++ mongodb.sh
@@ -8,7 +8,7 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 echo -e "$Y script started executing at $N $TIMESTAMP" &>> $LOGFILE
 VALIDATE() {
-    if [ $1-ne 0 ]
+    if [ $1 -ne 0 ]
     then
         echo -e " $2 ....$R FAILED $N "
         exit 1
