a721ac0be85141c628bb0ebccde062118b1449e5
aws-devops26 <srivalli26.najara@gmail.com>
2024-08-17 22:36:42 +0530

    roboshop shell scripting

--- web.sh
+++ web.sh
@@ -31,8 +31,6 @@ VALIDATE $? "installing nginx"
 systemctl enable nginx &>> $LOGFILE
 VALIDATE $? " nginx enabiling "
 
-systemctl start nginx &>> $LOGFILE
-VALIDATE $? "nginx starting"
 
 rm -rf /usr/share/nginx/html/* &>> $LOGFILE
 VALIDATE $? " Removing the default content that web server is serving."
