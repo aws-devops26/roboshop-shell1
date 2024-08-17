8cccd3156690fc1f652d07ce04daa1ac10f04b1a
aws-devops26 <srivalli26.najara@gmail.com>
2024-08-17 14:09:02 +0530

    catalogue roboshop

--- catalouge.sh
+++ catalouge.sh
@@ -26,33 +26,47 @@ else
 fi
 dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? " disabiling nodejs "
+
 dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? " enabiling nodejs:18 "
+
 dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? " installing node js "
+
 useradd roboshop &>> $LOGFILE
 VALIDATE $? "adding user roboshop"
+
 mkdir /app &>> $LOGFILE
 VALIDATE $? " created app directory"
+
 curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 VALIDATE $? " downloading catalouge application"
+
 cd /app 
 unzip /tmp/catalogue.zip &>> $LOGFILE
 VALIDATE $? " unzipping catalouge"
+
 npm install  &>> $LOGFILE
 VALIDATE $? " installing dependencies"
+
 cp /devops/repos/roboshop-shell1/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? " copying catalouge service "
+
 systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? " catalouge demon reload"
+
 systemctl enable catalouge &>> $LOGFILE
 VALIDATE $? " catalouge enabiling "
+
 systemctl start catalouge &>> $LOGFILE
 VALIDATE $? " catalouge start"
+
 cp /devops/repos/roboshop-shell1/mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 VALIDATE $? " copying mongodb repo"
+
 dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? " installing mongodb client"
+
 mongo --host $MONGODB_HOST </app/schema/catalogue.js &>> LOGFILE
 VALIDATE $? "loading catalouge data into mongodb"
 
