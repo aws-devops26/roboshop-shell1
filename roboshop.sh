bed4d036186cbf389090cfae15bed3255acfed96
aws-devops26 <srivalli26.najara@gmail.com>
2024-09-13 21:29:22 +0530

    roboshop shell scripting

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@ AMI=ami-0b4f379183e5706b9
 SG_ID=sg-0541e7f1344e449e2
 INSTANCES=("mongodb" "redis" "rabbitmq" "mysql" "catalogue" "user" "cart" 
 "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z025434420MOIOXIRVMIF
+DOMAIN_NAME="awssrivalli.online"
 for i in "${INSTANCES[@]}"
 do
     echo "instances: $i"
@@ -15,5 +17,26 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids  sg-0541e7f1344e449e2 --tag-specifications "ResourceType=instance,
     Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+
+
+  #creating r53 record make sure delete exisisting record
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch "
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+    }]
+  }
+  "
 
 done
